1b2b33adcbd03adbc40c36b412b508a7c489f2a8
Ryan Johnson <ryan@258webco.com>
2018-03-06 17:12:43 -0600

    #3 - Pages and Navigation in Gatsby

---
+++ src/pages/about.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+export default class About extends Component {
+  render() {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>
+          Lorem ipsum dolor amet taxidermy migas lumbersexual freegan, forage
+          yuccie wayfarers woke vinyl tousled next level cold-pressed
+          microdosing snackwave lo-fi. Banjo vice single-origin coffee meggings,
+          distillery taxidermy small batch swag gastropub mumblecore green juice
+          +1. Bicycle rights tattooed hell of blue bottle typewriter godard
+          wayfarers poke mumblecore deep v hella heirloom authentic
+          chicharrones. +1 aesthetic pop-up, vinyl af artisan air plant
+          sartorial jean shorts subway tile keytar wolf. Yuccie ennui trust fund
+          jianbing shabby chic gochujang helvetica you probably haven't heard of
+          them chia art party portland disrupt. Raclette tumblr tote bag
+          succulents XOXO artisan kitsch four dollar toast readymade.
+        </p>
+      </div>
+    );
+  }
+}
