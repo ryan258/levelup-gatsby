129f88b5ba43df0db81fd6e86b215962d87b506d
Ryan Johnson <ryan@258webco.com>
2018-03-07 16:54:32 -0600

    #13 - Animated page transitions w/ the WEB ANIMATIONS API

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,14 +1,16 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import Link from 'gatsby-link';
 import Img from 'gatsby-image';
 import styled from 'styled-components';
+import logo from '../../images/logo.svg';
 
 const HeaderWrapper = styled.div`
   background: #524763;
   margin-bottom: 1.45rem;
   overflow: hidden;
   position: relative;
-  height: 70vh;
+  height: ${({ isHome }) => (isHome ? '70vh' : '20vh')};
   h1 {
     img {
       height: 80px;
@@ -24,48 +26,74 @@ const HeaderContainer = styled.div`
   z-index: 2;
 `;
 
-import logo from '../../images/logo.svg';
+export default class Header extends Component {
+  componentDidUpdate = (prevProps, prevState) => {
+    console.log(this.props.location.pathname);
+    const { location } = this.props;
+    if (location.pathname !== prevProps.location.pathname) {
+      if (this.props.location.pathname === '/') {
+        // console.log(this.wrapper);
+        this.wrapper.animate([{ height: '20vh' }, { height: '70vh' }], {
+          duration: 300,
+          fill: 'forwards',
+          easing: 'cubic-bezier(0.86, 0, 0.07, 1)',
+          iterations: 1
+        });
+      } else {
+        this.wrapper.animate([{ height: '70vh' }, { height: '20vh' }], {
+          duration: 300,
+          fill: 'forwards',
+          easing: 'cubic-bezier(0.86, 0, 0.07, 1)',
+          iterations: 1
+        });
+      }
+    }
+  };
 
-// console.log(logo);
+  render() {
+    const { data, location } = this.props;
 
-const Header = ({ data }) => (
-  <HeaderWrapper>
-    <HeaderContainer>
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: 'white',
-            textDecoration: 'none'
-          }}
-        >
-          <img src={logo} alt="Apple Logo" />
-        </Link>
-      </h1>
-      {/* <p>{data.site.siteMetadata.title}</p>
+    return (
+      <HeaderWrapper
+        isHome={location.pathname === '/'}
+        ref={wrapper => (this.wrapper = ReactDOM.findDOMNode(wrapper))}
+      >
+        <HeaderContainer>
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: 'white',
+                textDecoration: 'none'
+              }}
+            >
+              <img src={logo} alt="Apple Logo" />
+            </Link>
+          </h1>
+          {/* <p>{data.site.siteMetadata.title}</p>
       <p>{data.site.siteMetadata.desc}</p> */}
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-        </ul>
-      </nav>
-    </HeaderContainer>
-    <Img
-      style={{
-        position: 'absolute',
-        left: 0,
-        top: 0,
-        width: '100%',
-        height: '100%'
-      }}
-      sizes={data.background.sizes}
-    />
-  </HeaderWrapper>
-);
-
-export default Header;
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+        </HeaderContainer>
+        <Img
+          style={{
+            position: 'absolute',
+            left: 0,
+            top: 0,
+            width: '100%',
+            height: '100%'
+          }}
+          sizes={data.background.sizes}
+        />
+      </HeaderWrapper>
+    );
+  }
+}
