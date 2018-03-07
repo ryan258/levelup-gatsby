44416680bf348a502ef426864bb01e1032800b77
Ryan Johnson <ryan@258webco.com>
2018-03-06 21:55:28 -0600

    #9 - How to Query Markdown Files in Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,10 +9,18 @@ module.exports = {
     {
       resolve: 'gatsby-source-filesystem',
       options: {
+        name: 'src',
+        path: `${__dirname}/src/`
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
         name: 'img',
         path: `${__dirname}/src/images`
       }
     },
+    'gatsby-transformer-remark',
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp'
   ]
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,14 +3,15 @@ import Link from 'gatsby-link';
 
 const IndexPage = ({ data }) => (
   <div>
-    <h1>Hi Bufalo!</h1>
-
     <p>{data.site.siteMetadata.title}</p>
     <p>{data.site.siteMetadata.desc}</p>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    {data.allMarkdownRemark.edges.map(({ node }) => {
+      return <PostListing post={node} />;
+    })}
   </div>
 );
+
+const PostListing = () => <div>hello</div>;
 
 export default IndexPage;
 
@@ -20,6 +21,17 @@ export const query = graphql`
       siteMetadata {
         title
         desc
+      }
+    }
+    allMarkdownRemark {
+      edges {
+        node {
+          frontmatter {
+            title
+            date(formatString: "MMMM DD YYYY")
+          }
+          html
+        }
       }
     }
   }
