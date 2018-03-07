552da45dee4bba2ea96543a9b68aa3e8964f195c
Ryan Johnson <ryan@258webco.com>
2018-03-06 19:34:34 -0600

    #7 - Image Transformation and Manipulation in Gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,17 @@ module.exports = {
     title: 'Gatsby Default Starter',
     desc: 'A new blog about apples!'
   },
-  plugins: ['gatsby-plugin-react-helmet', 'gatsby-plugin-styled-components']
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'img',
+        path: `${__dirname}/src/images`
+      }
+    },
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-sharp'
+  ]
 };
